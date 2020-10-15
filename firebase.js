7f65561460db249543a3bafb697ab9af157adef5
Francis Pegenia <francispegenia15@gmail.com>
2020-10-15 20:16:10 +0800

    header is done

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDlxYuVVMyGx14eB7YPYqfrwy0ATt3qbao",
+    authDomain: "clone-1c85c.firebaseapp.com",
+    databaseURL: "https://clone-1c85c.firebaseio.com",
+    projectId: "clone-1c85c",
+    storageBucket: "clone-1c85c.appspot.com",
+    messagingSenderId: "135625878790",
+    appId: "1:135625878790:web:9076fc38fac145f3568287"
+  };
